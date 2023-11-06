17712f2da39e9e26afc8cf6688bc16d5129a7d37
josep cos <josep.cos@vegeta.cima.fcen.uba.ar>
2023-11-06 18:20:40 -0300

    first commit

---
+++ esmval_diagnostic.py
@@ -0,0 +1,76 @@
+"""Python example diagnostic."""
+import os
+import logging
+from pathlib import Path
+from pprint import pformat
+import matplotlib.pyplot as plt
+import iris.quickplot as qplt
+import iris.plot as iplt
+
+import iris
+
+from esmvaltool.diag_scripts.shared import (
+    group_metadata,
+    run_diagnostic,
+    select_metadata,
+    sorted_metadata,
+)
+
+logger = logging.getLogger(Path(__file__).stem)
+
+
+def plot_diagnostic(groups, cfg):
+    """Create diagnostic data and plot it."""
+    plt.figure()
+    for attributes in groups:
+        logger.info("Processing dataset %s", attributes['dataset'])
+        input_file = attributes['filename']
+        cube = iris.load_cube(input_file)
+        dims = len(cube.shape)
+        if dims == 2 and attributes['dataset'] == "MultiModelMean":
+            qplt.pcolormesh(cube, cmap=cfg["cmap"])
+            plt.gca().coastlines()
+        elif dims == 1:
+            time_coord = [i.year for i in cube.coord("time").units.num2date(cube.coord("time").points)]
+            if attributes['dataset'] == "MultiModelMean":
+                plt.plot(time_coord, cube.data, color="black")
+            else:
+                plt.plot(time_coord, cube.data, alpha=0.3, color="grey")
+
+    title = cube.long_name
+    plt.ylabel(f"{cube.standard_name} / degC")
+    plt.title(attributes['caption'].format(title=title.lower()))
+    plt.tight_layout()
+    plt.savefig(os.path.join(cfg["plot_dir"], attributes["savefig"]), bbox_inches="tight")
+
+def main(cfg):
+    """Compute the time average for each input dataset."""
+    # Get a description of the preprocessed data that we will use as input.
+    input_data = cfg['input_data'].values()
+
+    # Demonstrate use of metadata access convenience functions.
+    selection = select_metadata(input_data, short_name='tas', project='CMIP6')
+    logger.info("Example of how to select only CMIP5 temperature data:\n%s",
+                pformat(selection))
+
+    selection = sorted_metadata(selection, sort='dataset')
+    logger.info("Example of how to sort this selection by dataset:\n%s",
+                pformat(selection))
+
+    grouped_input_data = group_metadata(input_data,
+                                        'variable_group',
+                                        sort='dataset')
+    logger.info(
+        "Example of how to group and sort input data by variable groups from "
+        "the recipe:\n%s", pformat(grouped_input_data))
+
+    # Example of how to loop over variables/datasets in alphabetical order
+    groups = group_metadata(input_data, 'variable_group', sort='dataset')
+    for group_name in groups:
+        logger.info("Processing variable %s", group_name)
+        plot_diagnostic(groups[group_name], cfg)
+
+
+if __name__ == '__main__':
+    with run_diagnostic() as config:
+        main(config)
---
+++ filler.py
@@ -0,0 +1,53 @@
+import glob
+import os
+import yaml
+
+root = "/shera/datos/CMIP/CMIP6"
+exps = ["historical", "ssp585"]
+
+vars = ["tas"] #, "tos", "pr"]
+datasets_dict = {v: {} for v in vars}
+for short_name in vars:
+    mip_prefix = "A"
+    if short_name == "tos":
+        mip_prefix = "O"
+    files = glob.glob(os.path.join(root, f"CMIP/*/*/historical/r1i*p*f*/{mip_prefix}mon/{short_name}/*/*/*2014*.nc"))
+    for file in files:
+        activity, institute, dataset, exp, ensemble, mip, short_name, grid = file.split(root)[-1].split("/")[1:-2]
+        if f"{dataset}_{ensemble}" in datasets_dict[short_name]:
+            if grid == "gn" and "gr" in datasets_dict[short_name][f"{dataset}_{ensemble}"]["grid"]:
+                datasets_dict[short_name][f"{dataset}_{ensemble}"] = {"institute": institute, "exp": exp, "ensemble": ensemble, "mip": mip, "grid": grid, "dataset": dataset}
+        else:
+            datasets_dict[short_name][f"{dataset}_{ensemble}"] = {"institute": institute, "exp": exp, "ensemble": ensemble, "mip": mip, "grid": grid, "dataset": dataset}
+
+# in exp ssp585
+scenario_exp = "ssp585"
+for short_name in vars:
+    mip_prefix = "A"
+    if short_name == "tos":
+        mip_prefix = "O"
+    popers = []
+    for alias, meta in datasets_dict[short_name].items():
+        file = glob.glob(os.path.join(root, f"ScenarioMIP/*/{meta['dataset']}/{scenario_exp}/{meta['ensemble']}/{mip_prefix}mon/{short_name}/{meta['grid']}/*/*2015*.nc"))
+        if len(file) == 0:
+            popers.append(alias)
+    for pop in popers:
+        datasets_dict[short_name].pop(pop)
+
+# If you want to keep only the runs that exist for all vars:
+aliases = set(datasets_dict[vars[0]].keys()) & set(datasets_dict[vars[1]].keys()) & set(datasets_dict[vars[2]].keys())
+datasets_recipe_dict = {}
+for short_name in vars:
+    poper = [alias for alias in datasets_dict[short_name] if alias not in aliases]
+    keys = list(datasets_dict[short_name].keys())
+    for alias in keys:
+        if alias not in aliases:
+            datasets_dict[short_name].pop(alias, None)
+        else:
+            datasets_dict[short_name][alias].pop("institute", None)
+            datasets_dict[short_name][alias]["exp"] = ["historical", "ssp585"]
+            datasets_dict[short_name][alias]["project"] = "CMIP6"
+    datasets_recipe_dict[f"datasets_{short_name}"] = list(datasets_dict[short_name].values())
+
+with open("/home/josep.cos/es-esmvaltool/common-tools/projections/recipes/datasets_filled.yml", "w") as f:
+    yaml.dump(datasets_recipe_dict, f, default_flow_style=False)
