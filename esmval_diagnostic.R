e6be72d85ecf33b60a3d2dc59b8da09f6f95cc1a
pepcos <josep.cos@bsc.es>
2023-11-15 12:09:12 -0300

    ejemplo R

--- esmval_diagnostic.R
+++ esmval_diagnostic.R
@@ -3,42 +3,26 @@ library(ncdf4)
 library(RColorBrewer)
 library(ggplot2)
 
-#Directory information
-setwd("/esarchive/scratch/molmo/")
-OUTPUTS <- "/esarchive/scratch/molmo/output/"
-
-#Useful functions
-png.save <- function(x,filename,main,xlab,ylab){
-  png(filename = paste(filename,".png",sep=""))
-  plot(x,type="o",main=main,xlab = xlab,ylab=ylab)
-  dev.off()
-}
 ################################################################################
 #Load ESMValTool stuff
 args <- commandArgs(trailingOnly = TRUE)
-#params <- read_yaml(args[1])
-setwd("/esarchive/scratch/molmo/output/")
-params <- read_yaml(paste(list.dirs(recursive = F)[length(list.dirs(recursive = F))],"/run/diagnostic_1/script1/settings.yml",sep=""))
+params <- read_yaml(args[1])
 
+# define the directories
 plot_dir <- params$plot_dir
 run_dir <- params$run_dir
 work_dir <- params$work_dir
-
-# Create working dirs if they do not exist
-dir.create(plot_dir, recursive = TRUE)
 dir.create(run_dir, recursive = TRUE)
+dir.create(plot_dir, recursive = TRUE)
 dir.create(work_dir, recursive = TRUE)
 
-metadata1 <- read_yaml(params$input_files[1])
-
-for(mod in 1:length(metadata1)){
-nc <- nc_open(metadata1[[mod]]$filename)
-gcm <- metadata1[[mod]]$dataset
-
-################################################################################
-#Load SLP data
-message("Loading and preprocessing data...")
-nc_lon <- ncvar_get(nc,"lon")
-nc_lat <- ncvar_get(nc,"lat")
-nc_var <- ncvar_get(nc,"unknown") #x,y,time
-
+for (i in 1:length(params$input_files)){
+  metadata1 <- read_yaml(params$input_files[i])
+  for(j in 1:length(metadata1)){
+    nc <- nc_open(metadata1[[j]]$filename)
+    gcm <- metadata1[[j]]$dataset
+    var_group <- metadata1[[j]]$variable_group
+    print(paste("NetCDF of dataset", gcm, "in variable group", var_group, ":"))
+    print(nc)
+  }
+}
